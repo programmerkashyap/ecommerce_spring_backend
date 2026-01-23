8f3500f1056da55546a715c3ffc8c921e5e04683
Himanshu Kashyap <himanshukashyap3210@gmail.com>
2026-01-23 17:53:10 +0530

    23-01-2026

--- src/main/java/com/example/ecommerce_backend/controllers/ProductController.java
+++ src/main/java/com/example/ecommerce_backend/controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.example.ecommerce_backend.controllers;
 
 import com.example.ecommerce_backend.models.Products;
 import com.example.ecommerce_backend.repositories.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +20,7 @@ import java.util.Map;
 
 public class ProductController {
 
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
     ProductRepository productRepository;
     public ProductController(ProductRepository productRepository)
     {
@@ -84,6 +87,75 @@ public class ProductController {
                 "status", "success",
                 "message", "Product Deleted Successfully"
         ));
+    }
+
+    @PutMapping("/product")
+    public Products updateCategory(
+            @RequestParam("id") Long id,
+            @RequestParam("category") String category,
+            @RequestParam("name") String name,
+            @RequestParam("mrp") String mrp,
+            @RequestParam("saleprice") String saleprice,
+            @RequestParam("description") String description,
+            @RequestParam(value = "image1", required = false) MultipartFile image1,
+            @RequestParam(value = "image2", required = false) MultipartFile image2
+    )
+    {
+        String filename1 = null;
+        String filename2 = null;
+        String foldername = "upload/";
+
+        if(image1!=null)
+        {
+            // when user uploads first image
+            filename1 = System.currentTimeMillis() + "_"+image1.getOriginalFilename();
+            Path path1 = Paths.get(foldername, filename1);
+            try {
+                Files.write(path1, image1.getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if(image2!=null)
+        {
+            // when user uploads second image
+            filename2 = System.currentTimeMillis() + "_" + image2.getOriginalFilename();
+            Path path2 = Paths.get(foldername, filename2);
+            try {
+                Files.write(path2, image2.getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        Products data = productRepository.findById(id).orElse(null);
+
+        if(data!=null) {
+
+            data.setName(name);
+            data.setMrp(mrp);
+            data.setCategory(category);
+            data.setSaleprice(saleprice);
+            data.setDescription(description);
+
+            log.info("File Name 1 = " + filename1);
+            log.info("File Name 2 = " + filename2);
+
+            if (filename1 != null) {
+                data.setImage1(filename1);
+            }
+            if (filename2 != null) {
+                data.setImage2(filename2);
+            }
+
+            return productRepository.save(data);
+
+        }
+        else
+        {
+            return null;
+        }
     }
 
 
